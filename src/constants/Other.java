cceba7f49c2b075f9090e7c99611893f12aa109e
Rodchananat <rod8711@gmail.com>
2018-11-12 23:24:35 +0700

    added tile class for floor

--- src/constants/Other.java
+++
@@ -1,5 +0,0 @@
-package constants;
-
-public class Other {
-	public static int[][] dir = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
-}
---
+++ src/model/Tile.java
@@ -0,0 +1,6 @@
+package model;
+
+public class Tile {
+	private double speedModifier;
+	private boolean isPlaceable;
+}
