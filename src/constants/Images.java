c78ba97553433c8c6aa0f8fa8b8697f8bfdd0c55
Rodchananat <rod8711@gmail.com>
2018-11-20 01:33:07 +0700

    :bug: fixed bad file name => cannot run

--- src/constants/Images.java
+++ src/constants/Images.java
@@ -48,57 +48,10 @@ public class Images {
 	public static Image buttonPausePressed = new Image("ui/button/button_pause_pressed.png", 190, 45, true, true);
 	
 	public static Image pauseMenuPanel = new Image("ui/pause_menu_panel.png", 300, 300, true, true);
-	public static Image towerButton = new Image("ui/tower_button.png", 300, 300, true, true);
-	public static Image towerButtonPressed = new Image("ui/tower_button_pressed.png", 300, 300, true, true);
+	public static Image towerButton = new Image("ui/tower_button.png", 64, 128, false, true);
+	public static Image towerButtonPressed = new Image("ui/tower_button_pressed.png", 64, 128, false, true);
 	
-	static {
-		bush1 = new Image("bush/bush1.png", 64, 64, true, true);
-		tile1 = new Image("tile/tile1.png", 64, 64, true, true);
-		tile2 = new Image("tile/tile2.png", 64, 64, true, true);
-		tileUnplaceable = new Image("tile/tileUnplaceable.png", 64, 64, true, true);
-		tileUnplaceable2 = new Image("tile/tileUnplaceable.png", 256, 256, true, true);
-		
-		bear = new Image("monster/bear.png", 64, 64, true, true);
-		elephant = new Image("monster/elephant.png", 64, 64, true, true);
-		moose = new Image("monster/moose.png", 64, 64, true, true);
-		normalTower = new Image("tower/tower1.png", 64, 64, true, true);
-		bombTower = new Image("tower/missile1.png", 64, 64, true, true);
-		fireTower = new Image("tower/flame.png", 64, 64, true, true);
-		iceTower = new Image("tower/ice.png", 64, 64, true, true);
-		normalBullet = new Image("projectile/normalBullet.png", 30, 30, true, true);
-		bomb = new Image("projectile/bomb.png", 30, 30, true, true);
-		fireBullet = new Image("projectile/fireBullet.png", 30, 30, true, true);
-		iceBullet = new Image("projectile/iceBullet.png", 30, 30, true, true);
-		
-		attackIcon = new Image("icon/attack.png", 32, 32, true, true);
-		bombIcon = new Image("icon/bomb.png", 32, 32, true, true);
-		cooldownIcon = new Image("icon/cooldown.png", 32, 32, true, true);
-		targetIcon = new Image("icon/target.png", 32, 32, true, true);
-		liveIcon = new Image("icon/live.png", 32, 32, true, true);
-		coinIcon = new Image("icon/coin.png", 32, 32, true, true);
-		infoIcon = new Image("icon/info.png", 32, 32, true, true);
-		
-		buttonSell = new Image("ui/button/button_sell.png", 190, 49, true, true);
-		buttonSellDisabled = new Image("ui/button/button_sell_disabled.png", 190, 45, true, true);
-		buttonSellHover = new Image("ui/button/button_sell_hover.png", 190, 45, true, true);
-		buttonSellPressed = new Image("ui/button/button_sell_pressed.png", 190, 45, true, true);
-		buttonUpgrade = new Image("ui/button/button_upgrade.png", 190, 49, true, true);
-		buttonUpgradeDisabled = new Image("ui/button/button_upgrade_disabled.png", 190, 49, true, true);
-		buttonUpgradeHover = new Image("ui/button/button_upgrade_hover.png", 190, 49, true, true);
-		buttonUpgradePressed = new Image("ui/button/button_upgrade_pressed.png", 190, 45, true, true);
-		buttonNext = new Image("ui/button/button_next.png", 190, 49, true, true);
-		buttonNextDisabled = new Image("ui/button/button_next_disabled.png", 190, 49, true, true);
-		buttonNextHover = new Image("ui/button/button_next_hover.png", 190, 49, true, true);
-		buttonNextPressed = new Image("ui/button/button_next_pressed.png", 190, 45, true, true);
-		buttonPause = new Image("ui/button/button_pause.png", 190, 49, true, true);
-		buttonPauseDisabled = new Image("ui/button/button_pause_disabled.png", 190, 49, true, true);
-		buttonPauseHover = new Image("ui/button/button_pause_hover.png", 190, 49, true, true);
-		buttonPausePressed = new Image("ui/button/button_pause_pressed.png", 190, 45, true, true);
-		
-		pauseMenuPanel = new Image("ui/pause_menu_panel.png", 300, 300, true, true);
-		towerButton = new Image("ui/tower_button.png", 300, 300, true, true);
-		towerButtonPressed = new Image("ui/tower_button_pressed.png", 300, 300, true, true);
-	}
+	
 	
 	
 	public static final Image[] explosion = new Image[16];
