52a570260ed526f75e6715692f98b868cac3935b
Rodchananat <rod8711@gmail.com>
2018-11-22 23:53:35 +0700

    :sparkles: now can preview monster path/whether you can place

--- src/ui/TowerMenu.java
+++ src/ui/TowerMenu.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import constants.Images;
 import constants.Numbers;
 import controller.GameManager;
+import controller.SuperManager;
 import exceptions.PathBlockedException;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
@@ -44,51 +45,52 @@ public class TowerMenu {
 		
 		GameManager gi = GameManager.getInstance();
 		cpp.pii tilePos = gi.getSelectedPosition();
-		try {
-			int choice = (int)Main.getGameScene().getButtonManager().getToggleGroup().getSelectedToggle().getUserData();
-			Tower floatingTower = gi.createTower(choice, tilePos.first, tilePos.second);
-			if (floatingTower.getX() < Numbers.COLUMNS && floatingTower.getY() < Numbers.ROWS) {
-				floatingTower.render(gc, true);							
-			}
-			cpp.pii start = gi.getStartTilePos(), end = gi.getEndTilePos();
-			
-			if (!tilePos.equals(lastPos)) {
-				lastPos.first = tilePos.first;
-				lastPos.second = tilePos.second;
-				if (gi.isPlaceable(tilePos.first, tilePos.second)) {
-					path = Algorithm2.BFS(end.first, end.second, start.first, start.second, tilePos);
-					isError = false;
+		int choice = SuperManager.getInstance().getTowerChoiceProp().get();
+		if (choice != -1) {	
+			try {
+				Tower floatingTower = gi.createTower(choice, tilePos.first, tilePos.second);
+				if (floatingTower.getX() < Numbers.COLUMNS && floatingTower.getY() < Numbers.ROWS) {
+					floatingTower.render(gc, true);							
 				}
-				else {
-					path = Algorithm2.BFS(end.first, end.second, start.first, start.second, new cpp.pii(-1, -1));	
-					isError = true;
+				cpp.pii start = gi.getStartTilePos(), end = gi.getEndTilePos();
+				
+				if (!tilePos.equals(lastPos)) {
+					lastPos.first = tilePos.first;
+					lastPos.second = tilePos.second;
+					if (gi.isPlaceable(tilePos.first, tilePos.second)) {
+						path = Algorithm2.BFS(end.first, end.second, start.first, start.second, tilePos);
+						isError = false;
+					}
+					else {
+						path = Algorithm2.BFS(end.first, end.second, start.first, start.second, new cpp.pii(-1, -1));	
+						isError = true;
+					}
 				}
-			}
-			
-			if (path != null) {
-				gc.setFill(new Color(0, 0, 0, 0.4)); // just dim
-				// need to copy
-				cpp.pii pos = new cpp.pii(start.first, start.second);
-				while (pos != null && !pos.equals(end)) {
+				
+				if (path != null) {
+					gc.setFill(new Color(0, 0, 0, 0.4)); // just dim
+					// need to copy
+					cpp.pii pos = new cpp.pii(start.first, start.second);
+					while (pos != null && !pos.equals(end)) {
+						gc.fillRect(pos.first*Numbers.TILE_SIZE, pos.second*Numbers.TILE_SIZE,
+								Numbers.TILE_SIZE, Numbers.TILE_SIZE);
+						pos = path[pos.first][pos.second];
+					}
 					gc.fillRect(pos.first*Numbers.TILE_SIZE, pos.second*Numbers.TILE_SIZE,
 							Numbers.TILE_SIZE, Numbers.TILE_SIZE);
-					pos = path[pos.first][pos.second];
 				}
-				gc.setFill(Color.BLACK);
+				if (isError) {
+					gc.setFill(Color.color(1, 0, 0, 0.7));
+					gc.fillRect(tilePos.first*Numbers.TILE_SIZE, tilePos.second*Numbers.TILE_SIZE,
+							Numbers.TILE_SIZE, Numbers.TILE_SIZE);
+				}
 			}
-			if (isError) {
+			catch (PathBlockedException e) {
+				isError = true;
 				gc.setFill(Color.color(1, 0, 0, 0.7));
 				gc.fillRect(tilePos.first*Numbers.TILE_SIZE, tilePos.second*Numbers.TILE_SIZE,
 						Numbers.TILE_SIZE, Numbers.TILE_SIZE);
 			}
-		}
-		catch (NullPointerException e) {
-		}
-		catch (PathBlockedException e) {
-			isError = true;
-			gc.setFill(Color.color(1, 0, 0, 0.7));
-			gc.fillRect(tilePos.first*Numbers.TILE_SIZE, tilePos.second*Numbers.TILE_SIZE,
-					Numbers.TILE_SIZE, Numbers.TILE_SIZE);
 		}
 
 	}
