cf891b43ff00af981ebcf14697679b8a59c98b8d
Rodchananat <rod8711@gmail.com>
2018-11-23 14:01:40 +0700

    :lipstick: Proof of concept of stacking menu style

--- src/ui/MainMenuScene.java
+++ src/ui/MainMenuScene.java
@@ -9,11 +9,15 @@ import controller.SuperManager;
 import javafx.animation.KeyFrame;
 import javafx.animation.KeyValue;
 import javafx.animation.Timeline;
+import javafx.animation.Transition;
 import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.input.KeyCode;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.util.Duration;
 import main.Main;
@@ -22,6 +26,8 @@ public class MainMenuScene extends Scene {
 	
 	private Timeline menuTick;
 	private Pane root;
+	private boolean showOverlay = false;
+	
 	
 	public MainMenuScene() {
 		super(new Pane(), Numbers.WIN_WIDTH, Numbers.WIN_HEIGHT);
@@ -53,7 +59,7 @@ public class MainMenuScene extends Scene {
 		menuTick.setCycleCount(Timeline.INDEFINITE);
 		menuTick.play();
 		
-
+		// tickle the superManager;
 		SuperManager.getInstance().getIsInGameProp().set(false);
 		SuperManager.getInstance().getCanSellProp().set(false);
 		SuperManager.getInstance().getCanUpgradeProp().set(false);
@@ -100,9 +106,55 @@ public class MainMenuScene extends Scene {
 			menuTick.pause(); // to make it playable, it should be resumable from other clas
 		});
 	
-		menus.getChildren().addAll(title, start, settings, newGame);
+		// TODO: change back
+//		menus.getChildren().addAll(title, start, settings, newGame);
+		menus.getChildren().addAll(title, start, settings);
 		menus.setMinWidth(Numbers.WIN_WIDTH);
 		root.getChildren().addAll(menus);
+		
+		// ---------------------------
+		Pane overlay = new Pane();
+		Canvas test = new Canvas(1600, 900);
+		overlay.getChildren().add(test);
+		Button back = ButtonMaker.make(0, 0, Images.buttonUpgrade, Images.buttonUpgradePressed, Other.normalButtonFont, "Back");
+		overlay.getChildren().add(back);
+		
+		overlay.setMouseTransparent(false);
+		overlay.setLayoutY(900);
+		
+		test.getGraphicsContext2D().setFill(Color.color(1, 1,0, 0.5));
+		test.getGraphicsContext2D().fillRect(0, 0, 1600, 900);
+		Timeline show = new Timeline(new KeyFrame(Duration.seconds(0.5), 
+				new KeyValue(overlay.layoutYProperty(), 0)
+		));
+		Timeline hide = new Timeline(new KeyFrame(Duration.seconds(0.5), 
+				new KeyValue(overlay.layoutYProperty(), 900)
+		));
+		test.setOnMouseClicked(e -> {
+			System.out.println("yoinked");
+			e.consume();
+		});
+		back.setOnAction(e -> {
+			hide.play();
+			showOverlay = false;
+		}); 
+		
+		setOnKeyPressed(e -> {
+			if (e.getCode() == KeyCode.M) {
+				if (!showOverlay) {
+					show.play();
+					System.out.println("play");
+//					toggle.setRate(1);
+				}
+				else {
+					hide.play();
+				}
+				showOverlay = !showOverlay;
+			}
+		});
+		overlay.getChildren().add(newGame);
+		root.getChildren().add(overlay);
+		// ------------------------
 	}
 	
 	public void resume() {
