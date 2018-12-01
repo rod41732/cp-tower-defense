cbad1d31ac033db218e95801139be424bcb9a49d
rod41732 <rod8711@gmail.com>
2018-12-02 02:11:33 +0700

    :lipstick: limit max width of tower info label

--- src/ui/game/TowerInfoPanel.java
+++ src/ui/game/TowerInfoPanel.java
@@ -1,13 +1,13 @@
 package ui.game;
 
 import constants.Images;
+import javafx.scene.control.Label;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundImage;
 import javafx.scene.layout.BackgroundPosition;
 import javafx.scene.layout.BackgroundSize;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
-import javafx.scene.text.Text;
 import ui.component.IconText;
 
 
@@ -19,17 +19,19 @@ public class TowerInfoPanel extends VBox {
 	
 	
 	
-	private Text titleText, descText;
+	private Label titleText, descText;
 	private IconText attackInfo, cooldownInfo, rangeInfo;
 	
 	public TowerInfoPanel(String towerName, String attack, String cooldown, String range, String desc) {
 		super(8);
-		titleText = new Text(towerName);
+		titleText = new Label(towerName);
+		titleText.setMaxWidth(256);
 		titleText.setFont(titleFont);
 		attackInfo = new IconText(Images.attackIcon, attack, textFont);
 		cooldownInfo = new IconText(Images.cooldownIcon, cooldown, textFont);
 		rangeInfo = new IconText(Images.targetIcon, range, textFont);
-		descText = new Text(desc);
+		descText = new Label(desc);
+		descText.setMaxWidth(256);
 		descText.setFont(textFont);
 		setBackground(new Background(new BackgroundImage(Images.towerInfoPanel, null, null, BackgroundPosition.CENTER, BackgroundSize.DEFAULT)));
 		getChildren().addAll(titleText, attackInfo, cooldownInfo, rangeInfo, descText);
