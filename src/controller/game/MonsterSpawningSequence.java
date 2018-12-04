76679ae96ebceb9efb599906870373195135cec0
Rodchananat <rod8711@gmail.com>
2018-12-05 00:52:58 +0700

    correctly stop spawning sequence

--- src/controller/game/MonsterSpawningSequence.java
+++ src/controller/game/MonsterSpawningSequence.java
@@ -10,25 +10,22 @@ import model.Monster;
 
 public class MonsterSpawningSequence extends Thread {
 	private static boolean isPaused;
-	
-	public MonsterSpawningSequence(int delay, int delay2, int repeat, ArrayList<Monster> monstersList) {
+	private static boolean shouldStop;
+	public MonsterSpawningSequence(int delay, int delay2, int repeat, Monster ...monstersList) {
 		super(new Runnable() {
 			@Override
 			public void run() {
-				for (int i=0; i<repeat; i++) {
+				for (int i=0; i<repeat && !shouldStop; i++) {
 					try {
-						while (isPaused) {
-							Thread.sleep(16);
-						}
 						Thread.sleep(delay/1000, delay%1000000);
 						for (Monster m: monstersList) {
 							Thread.sleep(delay2/1000, delay2%1000000);
 							while (isPaused) {
 								Thread.sleep(16); // pause when game paused;
 							}
-							System.out.println("spawned " + i);
+							if (shouldStop) break;
 							GameManager.getInstance().addMonster((Monster) m.clone());
-						}		
+						}	
 					}
 					catch (InterruptedException e) {
 						System.out.println("[W] A monster spawning sequence has been interrupted");
@@ -36,42 +33,14 @@ public class MonsterSpawningSequence extends Thread {
 					}
 	
 				}
-			}
-		});
-	}
-
-	public MonsterSpawningSequence(int delay, int delay2, int repeat, Monster ...monstersList) {
-		super(new Runnable() {
-			@Override
-			public void run() {
-				for (int i=0; i<repeat; i++) {
-					try {
-						Thread.sleep(delay/1000, delay%1000000);
-						for (Monster m: monstersList) {
-							Thread.sleep(delay2/1000, delay2%1000000);
-							while (isPaused) {
-								Thread.sleep(16); // pause when game paused;
-							}
-							if (Thread.currentThread().isInterrupted()) return;
-							System.out.println("spawned2 " + i);
-							GameManager.getInstance().addMonster((Monster) m.clone());
-						}	
-					}
-					catch (InterruptedException e) {
-						System.out.println("[W] A monster spawning sequence has been interrupted");
-						return;
-					}
-	
-				}
+				shouldStop = false;
 			}
 		});	
 	}
 	
 	@Override
 	public void interrupt() {
-		System.out.println("interrupted");
-		Thread.currentThread().interrupt();
-		System.out.println("is interrupted ? " + currentThread().isInterrupted());
+		shouldStop = true;
 		super.interrupt();
 	}
 	
@@ -84,6 +53,6 @@ public class MonsterSpawningSequence extends Thread {
 	
 	
 	public MonsterSpawningSequence(int delay) { // empty spawner for delay
-		this(delay, 1000, 1, new ArrayList<>()); 
+		this(delay, 1000, 1); 
 	}
 }
