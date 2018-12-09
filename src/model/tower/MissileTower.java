92060d9853f6f5883ef61b0162fb3d7fb4bb0721
rod41732 <rod8711@gmail.com>
2018-12-10 00:46:48 +0700

    missile tower now target 4 enemy

--- src/model/tower/MissileTower.java
+++ src/model/tower/MissileTower.java
@@ -1,11 +1,16 @@
 package model.tower;
 
 
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
 import constants.Images;
+import constants.Sounds;
 import constants.TowerStats;
 import controller.game.GameManager;
 import exceptions.FullyUpgradedException;
 import javafx.scene.image.Image;
+import model.Monster;
 import model.Tower;
 import model.projectile.HomingMissle;
 import model.projectile.Missile;
@@ -16,15 +21,40 @@ public class MissileTower extends Tower {
 
 	private static final Image DEFAULT_IMAGE = Images.bombTower;	
 	private double splashRadius;
+	private PriorityQueue<Monster> targets;
 	
+	
+	private class MonsterDistanceComparator implements Comparator<Monster> {
+		public Tower thisTower;
+		
+		public MonsterDistanceComparator(Tower thisTower) {
+			this.thisTower = thisTower;
+		}
+		@Override
+		public int compare(Monster o1, Monster o2) {
+			return -Double.compare(o1.distanceTo(thisTower), o2.distanceTo(thisTower)); // we want max heap instead of min heap
+		}
+		
+	}
 	
 	public MissileTower(double cellX, double cellY) {
 		super("Missile", DEFAULT_IMAGE, cellX, cellY);
 		this.splashRadius = (double) TowerStats.getData(typeName, "SplashRadius", level);
+		this.targets = new PriorityQueue<>(new MonsterDistanceComparator(this));
 	}
 
 	
-	
+	@Override
+	public boolean tryTarget(Monster m) {
+		if (super.tryTarget(m)) {
+			targets.add(m);				
+			if (targets.size() > 4) {
+				targets.remove();
+			}
+			return true;
+		}
+		return false;
+	}
 	
 	@Override
 	public boolean upgrade() throws FullyUpgradedException {
@@ -36,15 +66,13 @@ public class MissileTower extends Tower {
 	
 	public void fire() {
 		if (currentTarget == null) return;
+		Sounds.missileLaunch.play();
 		cpp.pff v = GameUtil.unitVector(this, currentTarget);
 		rotateTo(currentTarget);
-		if (level == 5) {
+		System.out.println("my target is" + currentTarget);
+		for (Monster m: targets) {
 			GameManager.getInstance().addProjectile(new 
-					HomingMissle(x, y, v.first*9, v.second*9, range, attack, splashRadius, currentTarget));
-		}
-		else {			
-			GameManager.getInstance().addProjectile(new 
-					Missile(x, y, v.first*9, v.second*9, range, attack, splashRadius));
+					HomingMissle(x, y, v.first*9, v.second*9, range, attack, splashRadius, m));			
 		}
 		currentCooldown = attackCooldown;
 	}
